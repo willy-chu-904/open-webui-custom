057277ba64e6e0b52ee4843990283bf544c6ffc0
willy-chu-904 <willy23659@gmail.com>
2026-03-02 01:26:58 +0800

    Update +layout.svelte

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -145,7 +145,7 @@
 			await goto('/auth');
 			return;
 		}
-		if (!['user', 'admin'].includes($user?.role)) {
+		if (!['user', 'admin', 'pro'].includes($user?.role)) {
 			return;
 		}
 
@@ -327,7 +327,7 @@
 		<div
 			class=" text-gray-700 dark:text-gray-100 bg-white dark:bg-gray-900 h-screen max-h-[100dvh] overflow-auto flex flex-row justify-end"
 		>
-			{#if !['user', 'admin'].includes($user?.role)}
+			{#if !['user', 'admin', 'pro'].includes($user?.role)}
 				<AccountPending />
 			{:else}
 				{#if localDBChats.length > 0}
