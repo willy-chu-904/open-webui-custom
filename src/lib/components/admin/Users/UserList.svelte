5419951cb2a7f5731b047f7144ba7e23023fcd33
willy-chu-904 <willy23659@gmail.com>
2026-06-07 00:45:54 +0800

    Update UserList.svelte

--- src/lib/components/admin/Users/UserList.svelte
+++ src/lib/components/admin/Users/UserList.svelte
@@ -368,8 +368,7 @@
 								}}
 							>
 								<Badge
-									type={user.role === 'admin' ? 'info' : user.role === 'pro' ? 'warning' : 
-user.role === 'user' ? 'success' : 'muted'}
+									type={user.role === 'admin' ? 'info' : user.role === 'pro' ? 'warning' : user.role === 'user' ? 'success' : 'muted'}
 									content={$i18n.t(user.role)}
 								/>
 							</button>
