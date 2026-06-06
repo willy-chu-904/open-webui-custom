d728d9e29983fbf2ad3285f5d19f8154a72a69e2
willy-chu-904 <willy23659@gmail.com>
2026-06-06 17:14:54 +0800

    Update env.py

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -771,7 +771,7 @@ if LICENSE_PUBLIC_KEY:
 #WEBUI_NAME = os.getenv('WEBUI_NAME', 'Open WebUI')
 #if WEBUI_NAME != 'Open WebUI':
 #    WEBUI_NAME += ' (Open WebUI)'
- WEBUI_NAME = "Seulama"
+WEBUI_NAME = "Seulama"
 
 WEBUI_FAVICON_URL = 'https://openwebui.com/favicon.png'
 WEBUI_BUILD_HASH = os.getenv('WEBUI_BUILD_HASH', 'dev-build')
